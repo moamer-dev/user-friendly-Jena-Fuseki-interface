5e9062fd3e1959296d0986eaec21f164799fd6fe
Mohamed Amer <81626580+moamer-dev@users.noreply.github.com>
2023-03-16 07:36:24 +0100

    Update database.php

--- app/core/database.php
+++ app/core/database.php
@@ -43,30 +43,5 @@ class Database
 		return false;
 	}
 
-	public function create_tables()
-	{
-		//users table
-		$query = "
-
-			CREATE TABLE IF NOT EXISTS `users` (
-			 `id` int(11) NOT NULL AUTO_INCREMENT,
-			 `email` varchar(100) NOT NULL,
-			 `firstname` varchar(30) NOT NULL,
-			 `lastname` varchar(30) NOT NULL,
-			 `password` varchar(255) NOT NULL,
-			 `role` varchar(20) NOT NULL,
-			 `date` date DEFAULT NULL,
-			 PRIMARY KEY (`id`),
-			 KEY `email` (`email`),
-			 KEY `firstname` (`email`),
-			 KEY `lastname` (`email`),
-			 KEY `date` (`date`)
-			) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
-
-		";
-
-		$this->query($query);
-	}
-
 	
 }
